f84cbfb01475d485b1482d69c30d04547552bd08
abdelrahmanElhoshy <wizardooo88888@gmail.com>
2017-05-10 23:26:02 +0200

    trial17

--- mooc/app/controllers/application_controller.rb
+++ mooc/app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
     protected
 
         def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :date_of_birth, :is_female) }
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :date_of_birth, :is_female, :avatar) }
             devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:name, :email, :password) }
-            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :is_female, :date_of_birth) }
+            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :is_female, :date_of_birth, :avatar) }
         end
 end
